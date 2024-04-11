d99dd15a70d0084f3dd70ca9a42f791b9961ad36
JacobDev1 <contact@codepoems.eu>
2024-04-11 16:47:04 +0200

    test_pathing: Add getUniqueFilePath and getPathGIF

--- tests/core/test_pathing.py
+++ tests/core/test_pathing.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 import pytest
 from pathlib import Path
 
@@ -8,6 +10,34 @@ from core.pathing import (
     getOutputDir,
     isANSICompatible,
 )
+
+@pytest.mark.parametrize("file_name, file_ext, output_dir, add_random, isfile_side_effect, expected_path", [
+    # Base
+    ("image", "jxl", Path("/home/user/images"), False, [False], Path("/home/user/images/image.jxl")),
+    ("image", "jxl", Path("/home/user/images"), False, [True, False], Path("/home/user/images/image (1).jxl")),
+    ("image", "jxl", Path("/home/user/images"), False, [True, True, False], Path("/home/user/images/image (2).jxl")),
+    ("image (10)", "jxl", Path("/home/user/images"), False, [False], Path("/home/user/images/image (10).jxl")),
+    ("image (10)", "jxl", Path("/home/user/images"), False, [True, True, False], Path("/home/user/images/image (11).jxl")), # Needs two passes
+    
+    # Add Random
+    ("image", "jxl", Path("/home/user/images"), True, [False], Path("/home/user/images/image_abc.jxl")),
+    ("image", "jxl", Path("/home/user/images"), True, [True, False], Path("/home/user/images/image (1)_abc.jxl")),
+    ("image (3)", "jxl", Path("/home/user/images"), True, [True, True, False], Path("/home/user/images/image (4)_abc.jxl")),
+])
+def test_getUniqueFilePath(file_name, file_ext, output_dir, add_random, isfile_side_effect, expected_path):
+    with patch("os.path.isfile", side_effect=isfile_side_effect), \
+        patch("random.choice", side_effect=["a", "b", "c"]):
+        
+        assert getUniqueFilePath(str(output_dir), file_name, file_ext, add_random) == str(expected_path)
+
+@pytest.mark.parametrize("output_dir, item_name, duplicates, isfile_side_effect, expected_path", [
+    (Path("user/images"), "animated", "Rename", [False], Path("user/images/animated.png")),
+    (Path("user/images"), "animated", "Rename", [True, False], Path("user/images/animated (1).png")),
+    (Path("user/images"), "animated", "Replace", [True], Path("user/images/animated.png")),
+])
+def test_getPathGIF(output_dir, item_name, duplicates, isfile_side_effect, expected_path):
+    with patch("os.path.isfile", side_effect=isfile_side_effect):
+        assert getPathGIF(str(output_dir), item_name, duplicates) == str(expected_path)
 
 @pytest.mark.parametrize("file_format, extension", [
     ("JPEG XL", "jxl"),
