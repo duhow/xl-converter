32f417088f47337ce681a645135a3e6268b0c88a
JacobDev1 <contact@codepoems.eu>
2024-04-21 16:11:00 +0200

    settings_tab: Redesign

--- ui/settings_tab.py
+++ ui/settings_tab.py
@@ -10,6 +10,9 @@ from PySide6.QtWidgets import(
     QPushButton,
     QLabel,
     QSizePolicy,
+    QScrollArea,
+    QFormLayout,
+    QSpacerItem,
 )
 from PySide6.QtCore import(
     Signal,
@@ -30,83 +33,125 @@ class SettingsTab(QWidget):
     def __init__(self):
         super(SettingsTab, self).__init__()
 
-        tab_lt = QGridLayout()
-        self.setLayout(tab_lt)
+        self.main_lt = QGridLayout()
+        self.setLayout(self.main_lt)
 
         self.wm = WidgetManager("SettingsTab")
         self.signals = Signals()
 
-        # General group - widgets
-        self.dark_theme_cb = self.wm.addWidget("dark_theme_cb", QCheckBox("Dark Theme"))
+        # Categories
+        self.categories_sa = QScrollArea(self)
+        self.categories_w = QWidget()
+        self.categories_lt = QVBoxLayout(self.categories_w)
+
+        self.categories_w.setLayout(self.categories_lt)
+        self.categories_sa.setWidget(self.categories_w)
+        self.categories_sa.setWidgetResizable(True)
+
+        # Settings
+        self.settings_sa = QScrollArea(self)
+        self.settings_w = QWidget()
+        self.settings_lt = QFormLayout(self.settings_w)
+
+        self.settings_w.setLayout(self.categories_lt)
+        self.settings_sa.setWidget(self.settings_w)
+        self.settings_sa.setWidgetResizable(True)
+
+        # Settings - widgets
+        self.dark_theme_cb = self.wm.addWidget("dark_theme_cb", QCheckBox("Dark Theme", self))
         self.dark_theme_cb.toggled.connect(self.setDarkModeEnabled)
 
-        self.disable_downscaling_startup_cb = self.wm.addWidget("disable_downscaling_startup_cb", QCheckBox("Disable Downscaling on Startup"))
-        self.disable_delete_startup_cb = self.wm.addWidget("disable_delete_startup_cb", QCheckBox("Disable Delete Original on Startup"))
+        self.disable_downscaling_startup_cb = self.wm.addWidget("disable_downscaling_startup_cb", QCheckBox("Disable Downscaling on Startup", self))
+        self.disable_delete_startup_cb = self.wm.addWidget("disable_delete_startup_cb", QCheckBox("Disable Delete Original on Startup", self))
 
-        self.no_exceptions_cb = self.wm.addWidget("no_exceptions_cb", QCheckBox("Disable Exception Popups"))
+        self.no_exceptions_cb = self.wm.addWidget("no_exceptions_cb", QCheckBox("Disable Exception Popups", self))
         self.no_exceptions_cb.toggled.connect(self.signals.no_exceptions)
 
-        self.no_sorting_cb = self.wm.addWidget("no_sorting_cb", QCheckBox("Input - Disable Sorting"))
+        self.no_sorting_cb = self.wm.addWidget("no_sorting_cb", QCheckBox("Input - Disable Sorting", self))
         self.no_sorting_cb.toggled.connect(self.signals.disable_sorting)
 
-        # General group - layout
-        gen_grp = QGroupBox("General")
-        gen_grp_lt = QVBoxLayout()
-        gen_grp.setLayout(gen_grp_lt)
-
-        gen_grp_lt.addWidget(self.dark_theme_cb)
-        gen_grp_lt.addWidget(self.disable_downscaling_startup_cb)
-        gen_grp_lt.addWidget(self.disable_delete_startup_cb)
-        gen_grp_lt.addWidget(self.no_exceptions_cb)
-        gen_grp_lt.addWidget(self.no_sorting_cb)
-
-        # Advanced group - widgets
-        self.enable_jxl_effort_10 = self.wm.addWidget("enable_jxl_effort_10", QCheckBox("JPEG XL - Enable Effort 10"))
+        self.enable_jxl_effort_10 = self.wm.addWidget("enable_jxl_effort_10", QCheckBox("JPEG XL - Enable Effort 10", self))
         self.enable_jxl_effort_10.clicked.connect(self.signals.enable_jxl_effort_10)
 
-        self.disable_jxl_utf8_check_cb = self.wm.addWidget("disable_jxl_utf8_check_cb", QCheckBox("libjxl - Disable UTF-8 Check"))
+        self.disable_jxl_utf8_check_cb = self.wm.addWidget("disable_jxl_utf8_check_cb", QCheckBox("libjxl - Disable UTF-8 Check", self))
 
-        self.disable_progressive_jpegli_cb = self.wm.addWidget("disable_progressive_jpegli_cb", QCheckBox("JPEGLI - Disable Progressive Scan"))
+        self.disable_progressive_jpegli_cb = self.wm.addWidget("disable_progressive_jpegli_cb", QCheckBox("JPEGLI - Disable Progressive Scan", self))
 
-        self.custom_resampling_cb = self.wm.addWidget("custom_resampling_cb", QCheckBox("Downscaling - Custom Resampling"))
+        self.custom_resampling_cb = self.wm.addWidget("custom_resampling_cb", QCheckBox("Downscaling - Custom Resampling", self))
         self.custom_resampling_cb.toggled.connect(self.signals.custom_resampling.emit)
 
-        # Advanced group - layout
-        conv_grp = QGroupBox("Advanced")
-        conv_grp_lt = QVBoxLayout()
-        conv_grp.setLayout(conv_grp_lt)
+        # Settings - layout
+        self.settings_lt.addRow(self.dark_theme_cb)
+        self.settings_lt.addRow(self.disable_downscaling_startup_cb)
+        self.settings_lt.addRow(self.disable_delete_startup_cb)
+        self.settings_lt.addRow(self.no_exceptions_cb)
+        self.settings_lt.addRow(self.no_sorting_cb)
+        self.settings_lt.addRow(self.enable_jxl_effort_10)
+        self.settings_lt.addRow(self.disable_jxl_utf8_check_cb)
+        self.settings_lt.addRow(self.custom_resampling_cb)
+        self.settings_lt.addRow(self.disable_progressive_jpegli_cb)
 
-        conv_grp_lt.addWidget(self.enable_jxl_effort_10)
-        
-        if os.name == "nt":
-            conv_grp_lt.addWidget(self.disable_jxl_utf8_check_cb)
-        
-        conv_grp_lt.addWidget(self.custom_resampling_cb)
+        # Categories - widgets
+        self.general_btn = QPushButton("General", self)
+        self.conversion_btn = QPushButton("Conversion", self)
+        self.advanced_btn  = QPushButton("Advanced", self)
+        self.restore_defaults_btn = QPushButton("Reset to Default", self)
 
-        conv_grp_lt.addWidget(self.disable_progressive_jpegli_cb)
-
-        # Bottom
-        self.restore_defaults_btn = QPushButton("Reset to Default")
+        self.general_btn.clicked.connect(lambda: self.changeCategory("General"))
+        self.conversion_btn.clicked.connect(lambda: self.changeCategory("Conversion"))
+        self.advanced_btn.clicked.connect(lambda: self.changeCategory("Advanced"))
         self.restore_defaults_btn.clicked.connect(self.resetToDefault)
-        tab_lt.addWidget(self.restore_defaults_btn,1,0,1,0)
 
-        # Size Policy
-        tab_lt.setAlignment(Qt.AlignTop)
+        # Categories - layout
+        self.categories_lt.addWidget(self.general_btn)
+        self.categories_lt.addWidget(self.conversion_btn)
+        self.categories_lt.addWidget(self.advanced_btn)
+        self.categories_lt.addItem(QSpacerItem(10, 10, QSizePolicy.Minimum, QSizePolicy.Expanding))
+        self.categories_lt.addWidget(self.restore_defaults_btn)
 
-        gen_grp.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        conv_grp.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        self.general_btn.setCheckable(True)
+        self.conversion_btn.setCheckable(True)
+        self.advanced_btn.setCheckable(True)
 
-        # Main Layout
-        tab_lt.addWidget(gen_grp, 0, 0)
-        tab_lt.addWidget(conv_grp, 0, 1)
+        # Main layout
+        self.main_lt.addWidget(self.categories_sa, 0, 0)
+        self.main_lt.addWidget(self.settings_sa, 0, 1)
+
+        self.main_lt.setColumnStretch(0, 3)
+        self.main_lt.setColumnStretch(1, 7)
 
         # Misc.
+        self.changeCategory("General")
         self.resetToDefault()
         self.wm.loadState()
 
         # Apply Settings
         self.setDarkModeEnabled(self.dark_theme_cb.isChecked())
-    
+
+    def changeCategory(self, category):
+        # Set vars
+        general = category == "General"
+        conversion = category == "Conversion"
+        advanced = category == "Advanced"
+
+        # Set checked
+        self.general_btn.setChecked(general)
+        self.conversion_btn.setChecked(conversion)
+        self.advanced_btn.setChecked(advanced)
+
+        # Set visible
+        self.dark_theme_cb.setVisible(general)
+        self.disable_downscaling_startup_cb.setVisible(general)
+        self.disable_delete_startup_cb.setVisible(general)
+        self.no_exceptions_cb.setVisible(general)
+        self.no_sorting_cb.setVisible(general)
+        
+        self.disable_progressive_jpegli_cb.setVisible(conversion)
+
+        self.enable_jxl_effort_10.setVisible(advanced)
+        self.custom_resampling_cb.setVisible(advanced)
+        self.disable_jxl_utf8_check_cb.setVisible(advanced if os.name == "nt" else False)
+
     def setDarkModeEnabled(self, enabled):
         if enabled:
             setTheme("dark")
